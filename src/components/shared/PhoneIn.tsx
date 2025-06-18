e141ee8ba11f77412901ba1262fd61e693ee324e
E3KONG <ethankong.atwork@gmail.com>
2025-06-18 17:09:06 +0800

    feat: phoneIn rive and scroll animation

--- src/components/shared/PhoneIn.tsx
+++ src/components/shared/PhoneIn.tsx
@@ -13,35 +13,43 @@ interface PhoneInProps {
 export const PhoneIn = ({ children }: PhoneInProps) => {
   const { rive, RiveComponent } = useRive({
     src: './assets/rive/shared-phonein.riv',
-    artboard: 'PhoneInMain',
+    artboard: 'Slide',
     stateMachines: 'State Machine 1',
     autoplay: true,
   });
 
-  const clickedInput = useStateMachineInput(rive, 'State Machine 1', 'Clicked');
+  const progress = useStateMachineInput(rive, 'State Machine 1', 'Progress');
   const phoneInRef = useRef<HTMLDivElement>(null);
 
   useGSAP(() => {
     gsap.registerPlugin(ScrollTrigger);
-    if (!rive || !clickedInput) return;
+    if (!rive || !progress) return;
+
+    gsap.set(progress, { value: 0 });
 
     ScrollTrigger.create({
       trigger: phoneInRef.current,
       start: 'top 50%',
-      end: 'bottom 50%',
-      onEnter: () => { if (clickedInput) { clickedInput.value = true; } },
-      onLeave: () => { if (clickedInput) { clickedInput.value = true; } },
-      onEnterBack: () => { if (clickedInput) { clickedInput.value = false; } },
-      onLeaveBack: () => { if (clickedInput) { clickedInput.value = false; } },
+      end: '+=500 50%',
+      pin: true,
+      scrub: 1,
+      markers: true,
+      onUpdate: (self) => {
+        progress.value = self.progress * 100 * 3;
+        if (progress.value >= 65) {
+          gsap.to('.phone-in-background', { opacity: 0, duration: 0.5, });
+        } else {
+          gsap.to('.phone-in-background', { opacity: 1, duration: 0.5, });
+        }
+      },
       id: 'phone-in-trigger'
     });
-  }, { scope: phoneInRef, dependencies: [clickedInput] });
+  }, { scope: phoneInRef, dependencies: [progress] });
 
   return (
     <div className="phone-in" ref={phoneInRef}>
-      <div className="phone-in-rive" style={{ height: '100px' }}>
-        <RiveComponent />
-      </div>
+      <div className="phone-in-rive"><RiveComponent /></div>
+      <div className="phone-in-background" />
       {children}
     </div>
   );
